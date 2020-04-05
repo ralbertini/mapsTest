18c5bba6f519cbef4c2275ff09982b9596d90331
Ronaldo Albertini <ronaldo.albertini@omni.com.br>
2020-04-05 16:40:22 -0300

    Dados básicos exibidos na tela de descrição da agencia

--- mapTest/mapTest/DetailsViewController.swift
+++ mapTest/mapTest/DetailsViewController.swift
@@ -12,15 +12,21 @@ import GooglePlaces
 class DetailsViewController: UIViewController {
 
     lazy var lbTitle: UILabel = UILabel()
+    
     lazy var lbAddress: UILabel = UILabel()
+    lazy var lbAddressDesc: UILabel = UILabel()
+    
     lazy var lbOpeningHours: UILabel = UILabel()
+
+    
+    lazy var logoImageView: UIImageView = UIImageView()
+    lazy var hoursStackView: UIStackView = UIStackView()
     
     var place: GMSPlace?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         self.setupViews()
         self.setupConstraints()
         self.loadData()
@@ -31,34 +37,90 @@ class DetailsViewController: UIViewController {
         
         self.lbTitle.text = place?.name
         self.lbAddress.text = place?.formattedAddress
-//        self.lbOpeningHours.text = place?.openingHours?.pe
+        
+        guard let periods = place?.openingHours?.periods else { return }
+        
+        
+        for hour in periods {
+        
+            let hourLabel: UILabel = UILabel()
+            hourLabel.font = UIFont(name: "Avenir Next", size: 16.0)
+            
+            let openString: String = "\(hour.openEvent.day.dayOfWeek()) Abre: \(hour.openEvent.time) - Fecha: \(hour.closeEvent!.time)"
+            
+            hourLabel.text = openString
+            
+            self.hoursStackView.addArrangedSubview(hourLabel)
+        }
     }
     
     private func setupViews() {
         
         self.view.backgroundColor = UIColor.white
         
+        self.lbTitle.font = UIFont(name: "Avenir Next", size: 22.0)
+        self.lbTitle.textAlignment = .center
+        
+        self.logoImageView.image = UIImage(named: "logo")
+        
         self.view.addSubview(self.lbTitle)
+        
+        self.lbAddress.font = UIFont(name: "Avenir Next", size: 16.0)
+        self.lbAddress.numberOfLines = 0
         self.view.addSubview(self.lbAddress)
+        
+        self.lbAddressDesc.font = UIFont(name: "Avenir Next", size: 18.0)
+        self.lbAddressDesc.text = "Endereço: "
+        self.view.addSubview(self.lbAddressDesc)
+        
+        self.hoursStackView.axis = .vertical
+        
+        self.view.addSubview(self.hoursStackView)
+        
+        self.lbOpeningHours.text = "Horário de funcionamento: "
+        self.lbOpeningHours.font = UIFont(name: "Avenir Next", size: 18.0)
+        
+        
         self.view.addSubview(self.lbOpeningHours)
+        self.view.addSubview(self.logoImageView)
     }
     
     private func setupConstraints() {
         
         self.lbTitle.snp.makeConstraints { (make) in
 
-            make.top.equalTo(self.topLayoutGuide.snp.bottom)
-            make.leadingMargin.equalToSuperview()
+            make.top.equalTo(self.logoImageView.snp.bottom).offset(20.0)
+            make.leading.equalToSuperview().offset(15.0)
+            make.trailing.equalToSuperview().offset(-15.0)
+        }
+        
+        self.lbAddressDesc.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(15.0)
+            make.top.equalTo(self.lbTitle.snp.bottom).offset(25.0)
+            make.trailing.equalToSuperview().offset(-15.0)
         }
         
         self.lbAddress.snp.makeConstraints { (make) in
-            make.leadingMargin.equalToSuperview()
-            make.top.equalTo(self.lbTitle.snp.bottomMargin)
+            make.leading.equalToSuperview().offset(15.0)
+            make.top.equalTo(self.lbAddressDesc.snp.bottom).offset(10.0)
+            make.trailing.equalToSuperview().offset(-15.0)
         }
         
         self.lbOpeningHours.snp.makeConstraints { (make) in
             make.leadingMargin.equalToSuperview()
-            make.top.equalTo(self.lbAddress.snp.bottomMargin)
+           make.top.equalTo(self.lbAddress.snp.bottom).offset(20.0)
+        }
+        
+        self.logoImageView.snp.makeConstraints { (make) in
+            make.height.width.equalTo(80.0)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+        }
+        
+        self.hoursStackView.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(15.0)
+            make.top.equalTo(self.lbOpeningHours.snp.bottom).offset(10.0)
+            make.trailing.equalToSuperview().offset(-15.0)
         }
     }
 
---
+++ mapTest/mapTest/GMSDayOfWeek+mapTest.swift
@@ -0,0 +1,33 @@
+//
+//  String+mapTest.swift
+//  mapTest
+//
+//  Created by Tomate Albertini on 05/04/20.
+//  Copyright © 2020 Ronaldo Albertini. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+
+extension GMSDayOfWeek {
+    
+    func dayOfWeek() -> String {
+        
+        switch self {
+        case .sunday:
+            return "Domingo"
+        case .monday:
+            return "Segunda-feira"
+        case .tuesday:
+            return "Terça-feira"
+        case .wednesday:
+            return "Quarta-feira"
+        case .thursday:
+            return "Quinta-feira"
+        case .friday:
+            return "Sexta-feira"
+        case .saturday:
+            return "Sábado"
+        }
+    }
+}
